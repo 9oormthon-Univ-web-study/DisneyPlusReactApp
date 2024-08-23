6d77722c9c56c0d76aa54971b63bad9d852fb9cb
kojesung <js95112345@gmail.com>
2024-08-23 14:30:59 +0900

    feat : 화살표 클릭 시 스크롤 기능 추가

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
 import { useCallback } from 'react';
+import './Row.css';
 
 const Row = ({ title, id, fetchUrl }) => {
     const [movies, setMovies] = useState([]);
@@ -22,22 +23,32 @@ const Row = ({ title, id, fetchUrl }) => {
         <div>
             <h2>{title}</h2>
             <div className="slider">
-                <div className="slider_arrow-left">
+                <div
+                    className="slider_arrow-left"
+                    onClick={() => {
+                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+                    }}
+                >
                     <span className="arrow">{'<'}</span>
                 </div>
-            </div>
-            <div id={id} className="row_posters">
-                {movies.map((movie) => (
-                    <img
-                        key={movie.id}
-                        className="row_poster"
-                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                        alt={movie.name}
-                    />
-                ))}
-            </div>
-            <div className="slider_arrow-right">
-                <span className="arrow">{'>'}</span>
+                <div id={id} className="row_posters">
+                    {movies.map((movie) => (
+                        <img
+                            key={movie.id}
+                            className="row_poster"
+                            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                            alt={movie.name}
+                        />
+                    ))}
+                </div>
+                <div
+                    className="slider_arrow-right"
+                    onClick={() => {
+                        document.getElementById(id).scrollLeft += window.innerWidth - 80;
+                    }}
+                >
+                    <span className="arrow">{'>'}</span>
+                </div>
             </div>
         </div>
     );
