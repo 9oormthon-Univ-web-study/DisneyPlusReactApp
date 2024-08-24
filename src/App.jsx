753e00fd21db844ad0242a440f2dcc6576101e86
kojesung <js95112345@gmail.com>
2024-08-24 15:03:33 +0900

    feat : 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-import styled from 'styled-components';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
-import Banner from './components/Banner';
-import Category from './components/Category';
-import Row from './components/Row';
-import request from './api/request';
+import Login from './pages/LoginPage/Login';
+import Main from './pages/MainPage/Main';
+import Detail from './pages/DetailPage/Detail';
+import Search from './pages/SearchPage/Search';
+
+const Layout = () => {
+    return (
+        <div>
+            <Nav />
+
+            <Outlet />
+        </div>
+    );
+};
 
 function App() {
     return (
-        <Container>
-            <Nav />
-            <Banner />
-            {/* 랜덤한 영화에 대한 정보가 배너로 등장 */}
-            <Category />
-            {/* 영화사들을 카테고리로 분류 */}
-            {/* 장르별로 영화들 리스트하는 Row */}
-            <Row title="Trending Now" id="TN" fetchUrl={request.fetchTrending} />
-            <Row title="Top Rated" id="TR" fetchUrl={request.fetchTopRated} />
-            <Row title="Action movies" id="AM" fetchUrl={request.fetchActionMovies} />
-            <Row title="Comedy Movie" id="CM" fetchUrl={request.fetchComedyMovies} />
-        </Container>
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Login />} />
+                    <Route path="main" element={<Main />} />
+                    <Route path=":movieId" element={<Detail />} />
+                    <Route path="search" element={<Search />} />
+                </Route>
+            </Routes>
+        </div>
     );
 }
 
 export default App;
-
-const Container = styled.main`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72px; //Nav크기만큼 밑에서 시작하기 위해
-    padding: 0 calc(3.5vw + 5px);
-
-    &:after {
-        background: url('/images/home-background.png') center center/cover no-repeat fixed;
-        content: '';
-        position: absolute;
-        inset: 0px;
-        opacity: 1;
-        z-index: -1;
-        margin: 0;
-    }
-`;
---
+++ src/pages/MainPage/Main.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+import Banner from '../../components/Banner';
+import Category from '../../components/Category';
+import Row from '../../components/Row';
+import request from './api/request';
+import styled from 'styled-components';
+
+const Main = () => {
+    return (
+        <Container>
+            <Nav />
+            <Banner />
+            {/* 랜덤한 영화에 대한 정보가 배너로 등장 */}
+            <Category />
+            {/* 영화사들을 카테고리로 분류 */}
+            {/* 장르별로 영화들 리스트하는 Row */}
+            <Row title="Trending Now" id="TN" fetchUrl={request.fetchTrending} />
+            <Row title="Top Rated" id="TR" fetchUrl={request.fetchTopRated} />
+            <Row title="Action movies" id="AM" fetchUrl={request.fetchActionMovies} />
+            <Row title="Comedy Movie" id="CM" fetchUrl={request.fetchComedyMovies} />
+        </Container>
+    );
+};
+
+export default Main;
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px; //Nav크기만큼 밑에서 시작하기 위해
+    padding: 0 calc(3.5vw + 5px);
+
+    &:after {
+        background: url('/images/home-background.png') center center/cover no-repeat fixed;
+        content: '';
+        position: absolute;
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+        margin: 0;
+    }
+`;
